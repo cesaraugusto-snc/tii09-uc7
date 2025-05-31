05c09e173b269afd4906d3b5d0431eb9de8e12dc
cesaraugusto-snc <cesaraugustodossantos1@hotmail.com>
2025-05-30 22:05:31 -0300

    Atualização Aula de API

--- 07/backend/DataBase.php
+++ 07/backend/DataBase.php
@@ -1,11 +1,9 @@
 <?php
-
 class Database
 {
     public static function getInstance()
     {
-        return new PDO("mysql:host=localhost;dbname=agenda", "root");
+        return new PDO("mysql:host=localhost;dbname=venda", "root", "");
     }
 }
-
 ?>
